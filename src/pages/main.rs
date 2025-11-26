7fbf7ac098abc299c1eed92631a5da2a55ed2d73
Gabriel Jõe <77448069+DrigsterI@users.noreply.github.com>
2025-11-26 15:56:38 +0200

    add simple usb support

--- src/pages/main.rs
+++ src/pages/main.rs
@@ -1,12 +1,25 @@
 use freya::prelude::*;
+use freya_radio::hooks::use_radio;
 
 #[derive(PartialEq)]
 pub struct Main {}
 impl Render for Main {
     fn render(&self) -> impl IntoElement {
+        let mut radio = use_radio::<crate::app::Data, crate::app::DataChannel>(
+            crate::app::DataChannel::DeviceUpdate,
+        );
+        let device = radio.read().device.clone().unwrap();
+        let handle = device.open();
+
         rect()
             .expanded()
             .center()
             .child(label().text("Device connected..."))
+            .child(label().text(format!("Device handle present: {:?}", device)))
+            .maybe_child(match handle {
+                Ok(_) => Some(label().text(format!("Device opened successfully! {:?}", device))),
+                Err(e) => Some(label().text(format!("Failed to open device: {}", e))),
+            })
+        // .child(Button::new().on_press(|| {}))
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,29 @@
-pub fn find_device(vid: u16, pid: u16) -> Option<String> {
-    match serialport::available_ports() {
-        Ok(ports) => {
-            for port in ports {
-                match &port.port_type {
-                    serialport::SerialPortType::UsbPort(info) => {
-                        if info.vid == vid && info.pid == pid {
-                            return Some(port.port_name);
+use std::error::Error;
+
+use rusb::{Context, Device, UsbContext};
+
+pub fn find_device(vid: u16, pid: u16) -> Option<Device<Context>> {
+    match Context::new() {
+        Ok(context) => match context.devices() {
+            Ok(devices) => {
+                for device in devices.iter() {
+                    if let Ok(desc) = device.device_descriptor() {
+                        if desc.vendor_id() == vid && desc.product_id() == pid {
+                            println!("Device found!");
+                            return Some(device);
                         }
                     }
-                    _ => {}
                 }
+                None
             }
+            Err(e) => {
+                println!("Failed to get device list");
+                None
+            }
+        },
+        Err(e) => {
+            println!("Failed to get device context");
             None
         }
-        Err(_) => None,
     }
 }
